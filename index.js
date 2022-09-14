c5b5ef9cf196967d43c08bbf4b13de0a076fa28f
njoro410 <brian.njoroge@student.moringaschool.com>
2022-09-14 10:49:40 +0300

    added a function that generates random string

--- index.js
+++ index.js
@@ -19,6 +19,21 @@ app.get('/', (req, res) => {
     res.json(data);
 });
 
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+ const generateRandomString = length => {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+
+
 app.get('/login', (req, res) => {
     const queryParams = queryString.stringify({
         client_id:CLIENT_ID,
