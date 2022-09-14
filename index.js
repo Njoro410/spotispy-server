c0381cc11c7afc1f46a60218c40189cce64365a0
njoro410 <brian.njoroge@student.moringaschool.com>
2022-09-14 10:12:18 +0300

    added dotenv dependency to access environment variables

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+import * as dotenv from 'dotenv' 
+dotenv.config()
 import express from 'express';
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, world');
+    // res.send('Hello, world');
+    const data = {
+        name: 'Brian',
+        isBoy: true,
+    };
+    res.json(data);
 });
+
+
 
 const port = 8888;
 app.listen(port, ()=>{
