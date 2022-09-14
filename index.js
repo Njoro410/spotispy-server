5a77c792c004a63a9987b77934cd8c55935614b4
njoro410 <brian.njoroge@student.moringaschool.com>
2022-09-14 11:03:27 +0300

    set up the POST request to https://accounts.spotify.com/api/token with the axios() method

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from 'express';
 const app = express();
 const port = 8888;
 import queryString from 'query-string';
-
+import axios from 'axios';
 
 const CLIENT_ID = process.env.CLIENT_ID
 const CLIENT_SECRET = process.env.CLIENT_SECRET
@@ -33,15 +33,52 @@ app.get('/', (req, res) => {
     return text;
   };
 
+  const stateKey = 'spotify_auth_state';
+
 
 app.get('/login', (req, res) => {
+    const state = generateRandomString(16);
+    res.cookie(stateKey, state)
+
+    const scope = 'user-read-private user-read-email'
+
     const queryParams = queryString.stringify({
         client_id:CLIENT_ID,
         response_type:'code',
         redirect_uri:REDIRECT_URI,
+        state:state,
+        scope:scope,
     })
 
     res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`)
+})
+
+app.get('/callback', (req, res) => {
+    const code = req.query.code || null;
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: queryString.stringify({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: REDIRECT_URI
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+    },
+  })
+    .then(response => {
+      if (response.status === 200) {
+        res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+      } else {
+        res.send(response);
+      }
+    })
+    .catch(error => {
+      res.send(error);
+    });
 })
 
 
