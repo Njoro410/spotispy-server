3726bebed5f38e98dbc34d50b4134a9fd4a2b5a6
njoro410 <brian.njoroge@student.moringaschool.com>
2022-09-15 13:03:27 +0300

    imported components into app.js

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,10 @@ import {
   Route,
 } from 'react-router-dom';
 import Welcome from "../src/components/Welcome";
+import TopArtists from './components/TopArtists';
+import TopTracks from './components/TopTracks';
+import Playlist from './components/Playlist';
+import Playlists from './components/Playlists';
 
 function App() {
   const [token, setToken] = useState(null);
@@ -37,6 +41,10 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Welcome token={token && token} logout={logout} profile={profile && profile} />} />
+          <Route path="/top-artists" element={<TopArtists/>}/>
+          <Route path="/top-tracks" element={<TopTracks/>}/>
+          <Route path="/playlists/:id" element={<Playlist/>}/>
+          <Route path="/playlists" element={<Playlists/>}/>
         </Routes>
       </Router>
     </>
