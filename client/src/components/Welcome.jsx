92927d3a0d2a9f93ba1dec85e72c41188babe6ff
njoro410 <brian.njoroge@student.moringaschool.com>
2022-09-15 13:37:56 +0300

    used styled components to sttyle login button

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,11 +1,24 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+
+const StyledLoginButton = styled.a`
+background-color: green;
+color: white;
+padding: 10px 20px;
+margin: 20px auto;
+border-radius: 30px;
+display: inline-block;
+`;
+
 const Welcome = ({ token, logout, profile }) => {
   return (
     <div className="App">
       <header className="App-header">
         {!token ? (
-          <a className="App-link" href="http://localhost:8888/login">
+          <StyledLoginButton className="App-link" href="http://localhost:8888/login">
             Log in to Spotify
-          </a>
+          </StyledLoginButton>
         ) : (
           <>
             <button onClick={logout}>Log Out</button>
