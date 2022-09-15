cde6f865126a1aefe15f31f4d18a9ec31e8bf914
njoro410 <brian.njoroge@student.moringaschool.com>
2022-09-15 10:39:49 +0300

    added axios default url headers

--- client/src/spotify.js
+++ client/src/spotify.js
@@ -117,3 +117,11 @@ const hasTokenExpired = () => {
   };
   
   export const accessToken = getAccessToken();
+
+  /**
+ * Axios global request headers
+ * https://github.com/axios/axios#global-axios-defaults
+ */
+axios.defaults.baseURL = 'https://api.spotify.com/v1';
+axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+axios.defaults.headers['Content-Type'] = 'application/json';
