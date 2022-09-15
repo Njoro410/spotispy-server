4b32b9733853e6c7df1277b6660e658f9a6ec751
njoro410 <brian.njoroge@student.moringaschool.com>
2022-09-15 12:49:52 +0300

    added user login code

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,29 @@
+const Welcome = ({ token, logout, profile }) => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {!token ? (
+          <a className="App-link" href="http://localhost:8888/login">
+            Log in to Spotify
+          </a>
+        ) : (
+          <>
+            <button onClick={logout}>Log Out</button>
+
+            {profile && (
+              <div>
+                <h1>{profile.display_name}</h1>
+                <p>{profile.followers.total} Followers</p>
+                {profile.images.length && profile.images[0].url && (
+                  <img src={profile.images[0].url} alt="Avatar" />
+                )}
+              </div>
+            )}
+          </>
+        )}
+      </header>
+    </div>
+  );
+};
+
+export default Welcome;
