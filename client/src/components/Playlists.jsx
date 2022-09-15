f566cfc5f637b869ac6628d7e85f625221493873
njoro410 <brian.njoroge@student.moringaschool.com>
2022-09-15 13:02:50 +0300

    created components for various data to be displayed

---
+++ client/src/components/Playlists.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Playlists = () => {
+  return (
+    <div className="app">
+      <header className="App-header">
+        <h1>Playlists</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Playlists;
---
+++ client/src/components/TopTracks.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TopTracks = () => {
+  return (
+    <div className="app">
+      <header className="App-header">
+        <h1>Top Tracks</h1>
+      </header>
+    </div>
+  );
+};
+
+export default TopTracks;
