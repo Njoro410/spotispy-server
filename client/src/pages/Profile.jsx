42d660b380c754a37cc36d525b3b6e0b389bbcd1
njoro410 <brian.njoroge@student.moringaschool.com>
2022-09-16 10:55:04 +0300

    added functions to fetch more data from spotify

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import { catchErrors } from "../utils";
+import { getCurrentUserProfile, getCurrentUserPlaylists, getTopArtists } from "../spotify";
+import SectionWrapper from "../components/SectionWrapper"
+import ArtistsGrid from "../components/ArtistsGrid"
+import { StyledHeader } from '../styles';
+
+const Profile = () => {
+  const [profile, setProfile] = useState(null);
+  const [playlists, setPlaylists] = useState(null);
+  const [topArtists, setTopArtist] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const userProfile = await getCurrentUserProfile();
+      setProfile(userProfile.data);
+
+      const userPlaylists = await getCurrentUserPlaylists();
+      setPlaylists(userPlaylists.data);
+
+      const userTopArtist = await getTopArtists();
+      setTopArtist(userTopArtist.data);
+    };
+
+    catchErrors(fetchData());
+  }, []);
+  return (
+    <>
+      {profile && (
+        <>
+          <StyledHeader type="user">
+            <div className="header__inner">
+              {profile.images.length && profile.images[0].url && (
+                <img
+                  className="header__img"
+                  src={profile.images[0].url}
+                  alt="Avatar"
+                />
+              )}
+              <div>
+                <div className="header__overline">Profile</div>
+                <h1 className="header__name">{profile.display_name}</h1>
+                <p className="header__meta">
+                  {playlists && (
+                    <span>
+                      {playlists.total} Playlist
+                      {playlists.total !== 1 ? "s" : ""}
+                    </span>
+                  )}
+                  <span>
+                    {profile.followers.total} Follower
+                    {profile.followers.total !== 1 ? "s" : ""}
+                  </span>
+                </p>
+              </div>
+            </div>
+          </StyledHeader>
+          {topArtists && (
+            <main>
+              <SectionWrapper title="Top artists this month" seeAllLink="/top-artists">
+                <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
+              </SectionWrapper>
+            </main>
+          )}
+        </>
+      )}
+    </>
+  );
+};
+
+export default Profile;
